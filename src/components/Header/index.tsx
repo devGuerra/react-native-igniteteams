55b602283396bff5c19c756343983f8c7a51cc42
Roberto Guerra <prosaka@hotmail.com>
2022-09-27 17:21:51 -0300

    :sparkles": Group card, header, highlight

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { CaretLeft } from "phosphor-react-native";
+
+import { Container, Logo, BackButton, BackIcon } from "./styles";
+import logoImg from "@assets/logo.png";
+
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
+  return (
+    <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+      <Logo source={logoImg} />
+    </Container>
+  );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,16 @@
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 import React from "react";
 import * as S from "./style";
 
 export function Groups() {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do ignite" />
+    </S.Container>
+  );
 }
