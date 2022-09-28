0bf5948113ac8339913676f37de2ea116bf536f3
Roberto Guerra <prosaka@hotmail.com>
2022-09-28 18:13:22 -0300

    :sparkles: Adicionando Async Storage

---
+++ src/storage/group/groupCreate.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GROUP_COLLECTION } from "@storage/storageConfig";
+
+import { groupsGetAll } from "./groupsGetAll";
+
+export async function groupCreate(newGroup: string) {
+  try {
+    const storedGroups = await groupsGetAll();
+    await AsyncStorage.setItem(
+      GROUP_COLLECTION,
+      JSON.stringify([...storedGroups, newGroup])
+    );
+  } catch (error) {
+    throw error;
+  }
+}
