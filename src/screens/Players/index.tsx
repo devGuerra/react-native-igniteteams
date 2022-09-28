8717f99ea61343a2e1db465a7cc01b044b0ec0d3
Roberto Guerra <prosaka@hotmail.com>
2022-09-28 00:47:16 -0300

    :sparkles: Lista de times

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,11 +4,15 @@ import ButtonIcon from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import Input from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
 
 export function Players() {
+  const [team, setTeam] = React.useState("Time A");
+  const [players, setPlayers] = React.useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,11 +21,26 @@ export function Players() {
         title="Nome da Turma"
         subtitle="Adicione a galera e separe os times"
       />
+
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A" isActived />
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActived={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
