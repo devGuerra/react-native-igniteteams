6f2be434a04f038950a2688bbf2cde80ec93f400
Roberto Guerra <prosaka@hotmail.com>
2022-09-28 01:51:26 -0300

    :sparkles: Glitch na navegação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
+import { View } from "react-native";
+import { useTheme } from "styled-components";
 
 export function Routes() {
+  const { COLORS } = useTheme();
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
 }
